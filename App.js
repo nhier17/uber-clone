893e1437083957eac7f4a12d4200ea353bc55460
Abraham <abrahamnhier97@gmail.com>
2024-01-29 15:42:39 +0300

    initial setup

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//set up redux
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Lets build uber</Text>
+      
     </View>
   );
 }
