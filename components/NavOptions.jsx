e20b8af2e3fb68e6666d548c07dfbdfc94b0957c
Abraham <abrahamnhier97@gmail.com>
2024-01-30 09:30:11 +0300

    building the nav options component

---
+++ components/NavOptions.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NavOptions = () => {
+  return (
+    <View>
+      <Text>NavOptions</Text>
+    </View>
+  )
+}
+
+export default NavOptions
+
